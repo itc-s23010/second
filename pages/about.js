505315522ae0581e1656a831f2367b14bdfe9ee8
itc-s23010 <s23010@std.it-college.ac.jp>
2024-02-24 07:17:10 +0000

    rokumade1

--- pages/about.js
+++ pages/about.js
@@ -1,3 +1,4 @@
+import Meta from 'components/meta'
 import Container from 'components/container'
 import Hero from 'components/hero'
 import PostBody from 'components/post-body'
@@ -9,10 +10,16 @@ import {
 } from 'components/two-column'
 import Image from 'next/image'
 import eyecatch from 'images/about.jpg'
-
 const About = () => {
   return (
     <Container>
+      <Meta
+        pageTitle='アバウト'
+        pageDesc='About development activites'
+        pageImg={eyecatch.src}
+        pageImgW={eyecatch.width}
+        pageImgH={eyecatch.heighy}
+      />
       <Hero title='About' subtitle='About development activities' />
 
       <figure>
@@ -20,7 +27,7 @@ const About = () => {
           src={eyecatch}
           alt=''
           layout='responsive'
-          size='(min-width: 1152px) 1152px,100vw'
+          sizes='(min-width: 1152px) 1152px, 100vw'
           priority
           placeholder='blur'
         />
@@ -29,18 +36,18 @@ const About = () => {
         <TwoColumnMain>
           <PostBody>
             <p>
-              Cubeが得意とする分野はモノづくりです。３次元から２次元の造形、プログラミングやデザインなど、様々な技術を組み合わせることによって社会や環境と結びつけるクリエイティブを提案し続けています。
+              Cubeが得意とする分野はモノづくりです。3次元から2次元の造形、プログラミングやデザインなど、さまざまな技術を組み合わせることによって社会や環境と結びつけるクリエイティブを提供し続けれいます。
             </p>
             <h2>モノづくりで目指していること</h2>
             <p>
-              モノづくりではデータの解析からクリエイティブまで幅広いことを担当しています。新しいことを取り込み取り入れながら、ユーザーにマッチした提案を実現するのが目標です。たくさんの開発・提供が数多くありますが、特にそこを磨く作業に力を入れています。
+              モノづくりではデータの解析からクリエイティブまで幅広いことを担当しています。新しいことを取り入れながら、ユーザーにマッチした提案を実施するのが目標です。たくさんの開発・提供が数多くありますが、特にそこを磨く作業に力を入れています。
             </p>
             <p>
-              単純に形にするだけでなく、作る過程や、なぜそのようにしたのかを大事にしながらものづくりをしています。毎回課題解決テーマを持って、「モノ」と向き合い制作をし、フィードバックしてもらうことで自分の中にあるもやもやを言葉にして「問い」への答えを出しています。
+              単純に形にするだけではなく、作る過程や、なぜそのようにしたのかを大事にしながらものづくりをしています。毎回課題解決テーマをもって「モノ」と向き合い制作をし、フィードバックしてもらうことで自分の中にあるモヤモヤを言葉にして「問い」への答えを出しています。
             </p>
             <h3>新しいことへのチャレンジ</h3>
             <p>
-              今までと違うものを作ることで愛着が湧いてきます。そこで興味を持ったことは小さなことでもいいから取り入れて、いいものを作れるようにしています。小さなヒントから新しいものを生み出すようなモノづくりは、これからも続けていきたいです。
+              今までと違うものを作ることで愛着が湧いてきます。そこで興味を持ったことは小さなことでもいいから取り入れて、良いものを作れるようにしていきます。小さなヒントから新しいものを生み出すようなモノづくりは、これからも続けていきます。
             </p>
           </PostBody>
         </TwoColumnMain>
@@ -52,4 +59,5 @@ const About = () => {
     </Container>
   )
 }
+
 export default About
